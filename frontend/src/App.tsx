98d7ba9aa273030946ab7b2a58c51043fd4ce6fc
AndersWoodruff <113076762+AndersWoodruff@users.noreply.github.com>
2024-10-27 15:16:49 -0400

    Fix components error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import TodoList from "./components/TodoList";
+import TodoList from "./TodoList";
 import './App.css';
 
 function App() {
